7f2807c9adbfb245173ba6455c3388c540c5fddd
VSchaidulin <vaclav1981@gmx.de>
2026-04-11 10:38:23 +0200

    Add_hw_19.2

--- src/test/java/com/qa67el/tests/CreateAccountRegisterTests.java
+++ src/test/java/com/qa67el/tests/CreateAccountRegisterTests.java
@@ -4,29 +4,33 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class CreateAccountRegisterTests extends TestBase {
 
-    @Test(enabled = false)
+    @Test
     public void newUserRegisterPositiveTest(){
-        int i = (int) ((System.currentTimeMillis()/1000)%3600); //für Homework
+        int i = (int) ((System.currentTimeMillis()/1000)%3600);
+
         clickOnRegisterLink();
-        fillLoginRegisterForm("FirstName", "LastName", "Password", "ConfirmPassword");
+
+        fillLoginRegisterForm("Wasja", "Pipetkin", "Aa12345!", "Aa12345!");
         type(By.name("Email"), "vaclav2026" + i + "@gmx.de");
-        click(By.name("register-button"));
-        Assert.assertTrue(isSingnButton());
 
+        click(By.id("register-button"));
+
+        Assert.assertTrue(isElementPresent(By.className("result")));
     }
-
 
     @Test
     public void existedUserRegisterNegativeTest(){
         clickOnRegisterLink();
-        fillLoginRegisterForm("FirstName", "LastName", "Password", "ConfirmPassword");
-        click(By.name("register-button"));
-        type();
-        
 
+        fillLoginRegisterForm("Wasja", "Pipetkin", "Aa12345!", "Aa12345!");
+        type(By.name("Email"), "vaclav2026@gmx.de");
+
+        click(By.id("register-button"));
+
+        Assert.assertTrue(
+                isElementPresent(By.cssSelector(".validation-summary-errors"))
+        );
     }
-
 }
--- src/test/java/com/qa67el/tests/TestBase.java
+++ src/test/java/com/qa67el/tests/TestBase.java
@@ -1,7 +1,6 @@
 package com.qa67el.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,22 +24,21 @@ public class TestBase {
     }
 
     public boolean isHomeComponentPresent(){
-        return driver.findElements(By.xpath("//*[@href='/register']")).size()>0;
+        return isElementPresent(By.cssSelector("[href='/register']"));
     }
 
     public boolean isElementPresent(By locator){
-        return driver.findElements(locator).size()>0;
+        return driver.findElements(locator).size() > 0;
     }
 
-    @AfterMethod(enabled = false)
+    @AfterMethod
     public void tearDown(){
         driver.quit();
     }
 
     public void type(By locator, String text) {
-        click(locator);
         driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(text);   //für Homework для многократного запуска
+        driver.findElement(locator).sendKeys(text);
     }
 
     public void click(By locator) {
@@ -48,45 +46,23 @@ public class TestBase {
     }
 
     public boolean isAlertPresent() {
-        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(20))
-                .until(ExpectedConditions.alertIsPresent());
-        if (alert == null) {
-            return false;
-        } else {
+        try {
+            new WebDriverWait(driver, Duration.ofSeconds(5))
+                    .until(ExpectedConditions.alertIsPresent());
             return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 
-    public boolean isSingnButton() {
-        return isElementPresent(By.id("#register-button"));
-    }
-
     public void fillLoginRegisterForm(String firstName, String lastName, String password, String confirmPassword) {
-        click(By.name(firstName));
-        driver.findElement(By.name(firstName)).clear();
-        driver.findElement(By.name(firstName)).sendKeys("Wasja");
-        click(By.name(lastName));
-        driver.findElement(By.name(lastName)).clear();
-        driver.findElement(By.name(lastName)).sendKeys("Pipetkin");
-        click(By.name(password));
-        driver.findElement(By.name(password)).clear();
-        driver.findElement(By.name(password)).sendKeys("Aa12345!");
-        click(By.name(confirmPassword));
-        driver.findElement(By.name(confirmPassword)).clear();
-        driver.findElement(By.name(confirmPassword)).sendKeys("Aa12345!");
+        type(By.name("FirstName"), firstName);
+        type(By.name("LastName"), lastName);
+        type(By.name("Password"), password);
+        type(By.name("ConfirmPassword"), confirmPassword);
     }
 
     public void clickOnRegisterLink() {
-        clickOnRegistrationButton();
-    }
-
-    public void clickOnRegistrationButton() {
         click(By.cssSelector("[href='/register']"));
-    }
-
-    public void type() {
-        click(By.name("Email"));
-        driver.findElement(By.name("Email")).clear();
-        driver.findElement(By.name("Email")).sendKeys("vaclav2026@gmx.de");
     }
 }
